36f9ce93761e4b8dea5d32aa0092a60b3616f9ed
James Hodgkinson <james@terminaloutcomes.com>
2021-01-27 16:12:39 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# homebrew-handbrakecli
+
+[HandBrakeCLI](https://handbrake.fr/downloads2.php) for homebrew
+
+Installation: 
+
+    brew tap terminaloutcomes/handbrakecli-beta && brew install handbrakecli-beta
---
+++ handbrakecli-beta.rb
@@ -0,0 +1,13 @@
+class Handbrakeclibeta < Formula
+  desc "HandBrake CLI Beta"
+  homepage "https://handbrake.fr/downloads2.php"
+  version "1.4.0-beta.1"
+  license "GPLv2"
+
+  url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrakeCLI-#{version}.dmg"
+  sha256 "701c8e63c75d21a5516c1fc98cbba45735fec66b59645ec2151744c7971331ae"
+  #name "HandBrakeCLI"
+  def install
+    bin.install "HandBrakeCLI"
+  end
+end
